1a9ba6b92b4ef0fc5c79416d56f700f18518c39c
Jacob Dent <jdent@tacyarg.com>
2019-12-09 09:03:33 -0400

    expose generalized api

--- index.js
+++ index.js
@@ -41,5 +41,15 @@ module.exports = (config = {}) => {
     memo[`${ticker}CreateAddress`] = tickerCreateAddress(ticker)
     memo[`${ticker}Logs`] = tickerLogs(ticker)
     return memo
-  }, {})
+  }, {
+    _createAddress: (ticker, address, callback, options) => {
+      const ca = tickerCreateAddress(ticker)
+      return ca(address, callback, options)
+    },
+    _getInfo: (ticker) => getTickerInfo(ticker),
+    _tickerLogs: (ticker, callback) => {
+      const ca = tickerLogs(ticker)
+      return ca(callback)
+    },
+  })
 }
