673ff4a05f81f035a3e0c486b91cd2f502dfb914
Jacob Dent <jdent@tacyarg.com>
2019-12-09 09:47:04 -0400

    methods

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ module.exports = (config = {}) => {
     memo[`${ticker}Logs`] = tickerLogs(ticker)
     return memo
   }, {
-    supportedTickers: () => config.supportedTickers,
+    getSupportedTickers: () => config.supportedTickers,
     _createAddress: (ticker, address, callback, options) => {
       const ca = tickerCreateAddress(ticker)
       return ca(address, callback, options)
